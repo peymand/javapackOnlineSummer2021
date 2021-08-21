6c385b1139b9e47571f7e443582688558681c4ad
peyman <peymandodangeh@gmail.com>
2021-08-21 17:03:22 +0430

    adding profile class adding dto class adding main class to test

---
+++ src/main/java/com/kahkeshan/ui/controllers/Main.java
@@ -0,0 +1,10 @@
+package com.kahkeshan.ui.controllers;
+
+import com.kahkeshan.ui.models.StudentDTO;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StudentDTO dto = new StudentDTO("ali","alavi","math");
+    }
+}
